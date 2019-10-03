894f3467d9b107f72421e85f561e99b206c0bf48
GallaFrancesco <francesco.galla3@gmail.com>
2019-10-03 13:30:07 +0200

    formatting

--- source/ctlstar/parser.d
+++ source/ctlstar/parser.d
@@ -37,15 +37,11 @@ unittest {
 	// example formulas taken from Principles of MC - Katoen - CTL* section
 	assert(parseOK("(AFG(#p2 > 0) or AGEF(#p2 < 2) )"));
 
-	immutable fm1 = "AG(XF(#p1 > 0) and !((#p2 <= 3) U G (#p1 == 1)))";
 	assert(parseOK("AG(XF(#p1 > 0) and !((#p2 <= 3) U G (#p1 == 1)))"));
 
-	immutable fm2 = "AXG!(#p4 < 400) and EFG((#p1 > 0) || A(true U (#p4 < 400)))";
 	assert(parseOK("AXG!(#p4 < 400) and EFG((#p1 > 0) || A(true U (#p4 < 400)))"));
 
-	immutable fm3 = "(AFG(#p2 > 234) or (AGEF(#p5 == 0)))";
 	assert(parseOK("(AFG(#p2 > 234) or (AGEF(#p5 == 0)))"));
 
-	immutable fm4 = "((EFG(#p1 == (8*4)) && AF(true U (#p32 < 1))))";
 	assert(parseOK("((EFG(#p1 == (8*4)) && AF(true U (#p32 < 1))))"));
 }
