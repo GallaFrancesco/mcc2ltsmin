785f6afe043c0a612e5392dc29cbdca789e4b9dd
Francesco Galla` <galla@di.unito.it>
2020-01-27 15:16:38 +0100

    fuzzer

--- source/main.d
+++ source/main.d
@@ -5,15 +5,21 @@ import ctlstar.parser;
 import std.stdio;
 import std.string;
 
-void main(string[] args)
+int main(string[] args)
 {
-	if(args.length < 2) {
-		writeln("Usage: ./ctlstar \"[formula]\"");
-		return;
+	if(args.length < 2 && args.length > 3) {
+		writeln("Usage: ./ctlstar [--fuzz] \"[formula]\"");
+		return 0;
 	}
 
-    string fuzzed = fuzz(args[1]).matches.join();
-    writeln(fuzzed);
+    string formula;
+    if(args[1] == "--fuzz") {
+        formula = fuzz(args[2]).matches.join();
+        writeln(formula);
+    } else {
+        formula = args[1];
+    }
 
-	auto pt = parse(fuzzed);
+	auto pt = parse(formula);
+    return !pt.successful;
 }
