07bf8f830fd2bea5c2583b7b107fc19486bc8f89
Francesco Galla` <galla@di.unito.it>
2021-02-19 15:51:36 +0100

    updated command line handling & readme

--- source/main.d
+++ source/main.d
@@ -3,26 +3,36 @@ module main;
 import ctlstar.parser;
 
 import std.stdio;
+import std.getopt;
 import std.string;
 import core.stdc.stdio;
 
 int main(string[] args)
 {
-	if(args.length < 2 || args.length > 3) {
-		writeln("Usage: ./mcc2ltsmin [--fuzz] \"[formula]\"");
-		return 0;
-	}
-
+    bool doFuzz = false;
     string formula;
-    if(args[1] == "--fuzz") {
-        formula = fuzz(args[2]).matches.join();
+
+    auto helpInformation = getopt(
+            args,
+            "query|q", "The LTL/CTL formula to be processed", &formula,
+            "fuzz|f","Generate a CTL* formula from a CTL one", &doFuzz);
+
+    if(helpInformation.helpWanted | formula.empty) {
+        defaultGetoptPrinter(
+            "Usage: ./mcc2ltsmin [--fuzz] --query|q \"[formula]\"",
+            helpInformation.options);
+        return 1;
+    }
+
+    if(doFuzz) {
+        formula = fuzz(formula).matches.join();
         writeln(formula);
-    } else {
-        formula = args[1];
     }
 
 	auto pt = parse(formula);
+
     if(!pt.successful) writeln(pt);
     else printf("\n");
+
     return !pt.successful;
 }
